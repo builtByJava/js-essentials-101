81eefcea96d8950afcfe25e43ab35e90500078e7
dwarcholak <>
2024-07-21 04:38:44 -0400

    added in control flow and conditional logic lab

---
+++ controlFlow/control_flow.js
@@ -0,0 +1,37 @@
+let userRole = "admin";
+let accessLevel;
+let isLoggedIn = true;
+let userMessage;
+let userType = "subscriber";
+let userCategory;
+
+if(isLoggedIn){
+    if(userRole === "admin"){
+        accessLevel = "full access granted";
+        userMessage = "Welcome, Admin!";
+    }else if(userRole === "manager"){
+        accessLevel = "limited access granted";
+        userMessage = "Welcome, User!";
+    }else{
+        accessLevel = "no access granted";
+        userMessage = "Please log in to access the system";
+    }
+}
+
+switch(userType){
+    case "admin":
+        userCategory = "Administrator";
+        break;
+    case "manager":
+        userCategory = "Manager";
+        break;
+    case "subscriber":
+        userCategory = "Subscriber";
+        break;
+    default: 
+        userCategory = "Unknown";
+}
+
+console.log("Access Level:" , accessLevel);
+console.log("User Message:" , userMessage);
+console.log("User Type:", userCategory);
